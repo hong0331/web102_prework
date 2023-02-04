36cc29161061eaceca29e10dbac54442ef5f764e
hong0331 <0331912@gmail.com>
2023-02-04 12:53:52 +0900

    level 5

--- index.js
+++ index.js
@@ -101,10 +101,12 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let unsatisfiedList=GAMES_JSON.filter( game =>
+        game.pledged<game.goal
+    );
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unsatisfiedList);
 }
 
 // show only games that are fully funded
@@ -112,10 +114,12 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let satisfedList=GAMES_JSON.filter(game => 
+        game.pledged >= game.goal
+    );
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(satisfedList);
 }
 
 // show all games
@@ -123,7 +127,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON)
 }
 
 // select each button in the "Our Games" section
@@ -132,7 +136,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click",filterUnfundedOnly)
+fundedBtn.addEventListener("click",filterFundedOnly)
+allBtn.addEventListener("click",showAllGames)
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
